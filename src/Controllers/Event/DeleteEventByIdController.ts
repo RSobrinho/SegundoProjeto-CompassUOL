121b985c00c991daabfb481479d4efece18d99a4
Breno Rodrigues <brenoferreira.bf8@gmail.com>
2023-03-09 09:30:45 -0400

    feat: delete by id route

--- src/Controllers/Event/DeleteEventByIdController.ts
+++ src/Controllers/Event/DeleteEventByIdController.ts
@@ -3,15 +3,16 @@ import { BaseError } from '../../Error/BaseError'
 import Event from '../../Models/eventModel'
 
 export class DeleteEventByIdController {
-  async handle (req: Request, res: Response): Promise<Response> {
-    const event = Event.findById(req.params)
+  async handle(req: Request, res: Response): Promise<Response> {
+    const event = await Event.findById( req.params.id )
+    console.log(event);
 
-    if (!event) {
-      throw new BaseError(409, 'No event find by this Id.')
-    }
+    if (!event) throw new BaseError(409, 'No event find by this Id.')
 
-    await Event.deleteOne({ _id: req.params })
+    await Event.deleteOne({ _id: req.params.id })
 
-    return res.status(204)
+    return res.status(204).end()
   }
 }
+
+export const deleteEventByIdController = new DeleteEventByIdController()
--- src/Routes/EventRouter.ts
+++ src/Routes/EventRouter.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express'
 import { CreateEventrController as CreateEventController } from '../Controllers/Event/createEventController'
+import { deleteEventByIdController } from '../Controllers/Event/DeleteEventByIdController'
 import { asyncHandler } from '../Error/Handler'
 
 const router = Router()
@@ -7,6 +8,11 @@ const router = Router()
 router.route('/')
   .post(asyncHandler((request: Request, response: Response) => {
     return CreateEventController.handle(request, response)
+  }))
+
+router.route('/:id')
+  .delete(asyncHandler((request: Request, response: Response) => {
+    return deleteEventByIdController.handle(request, response)
   }))
 
 export default router
