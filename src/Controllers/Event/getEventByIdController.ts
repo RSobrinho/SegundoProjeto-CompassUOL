3fa6bf3d6da8fe8d3e92b222981b834055e0036b
ShogoYK <shogonicolas@gmail.com>
2023-03-08 22:29:06 -0400

    feat: get event by id

---
+++ src/Controllers/Event/getEventByIdController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express'
+import { BaseError } from '../../Error/BaseError'
+import Event from '../../Models/eventModel'
+
+export class GetEventById {
+  async handle (req: Request, res: Response): Promise<Response> {
+    const event = await Event.findById(req.params.id)
+
+    if (!event) {
+      throw new BaseError(404, 'No event found by that ID')
+    }
+    return res.status(200).json({ status: 'success', event })
+  }
+}
