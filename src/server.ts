c289763985d0157dfdf68a5605df4c94c4b12562
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-03-09 10:58:31 -0400

    fix: change word casing

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import app from './App'
+import app from './app';
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Listening on ${port}`)
-})
+  console.log(`Listening on ${port}`);
+});
