c2deed6d325ef6c1f87842a84f5fc63e425dfc7f
Breno Rodrigues <brenoferreira.bf8@gmail.com>
2023-03-09 08:31:36 -0400

    fix: change CreateEventrController to CreateEventrController

--- src/Routes/EventRouter.ts
+++ src/Routes/EventRouter.ts
@@ -1,12 +1,12 @@
 import { Router, Request, Response } from 'express'
-import { CreateEventrController } from '../Controllers/Event/createEventController'
+import { CreateEventrController as CreateEventController } from '../Controllers/Event/createEventController'
 import { asyncHandler } from '../Error/Handler'
 
 const router = Router()
 
 router.route('/')
   .post(asyncHandler((request: Request, response: Response) => {
-    return CreateEventrController.handle(request, response)
+    return CreateEventController.handle(request, response)
   }))
 
 export default router
