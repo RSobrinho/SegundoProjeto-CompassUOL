58e020ce42ba6a2a5de2d1db4b0bf334a10f1452
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-03-09 11:01:47 -0400

    feat: add signin route

---
+++ src/Controllers/User/SignInUserController.ts
@@ -0,0 +1,29 @@
+import User from '../../Models/UserModel';
+import { ValidationError } from '../../Error/ValidationError';
+import { Request, Response } from 'express';
+
+export class SignInUserController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      throw new ValidationError(
+        'Validation Error: Email and password are required.'
+      );
+    }
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.verifyPass(password, user.password))) {
+      throw new ValidationError(
+        'Validation Error: Email or password not valid.'
+      );
+    }
+
+    return res
+      .status(200)
+      .json({ status: 'Success', message: 'User LoggedIn' });
+  }
+}
+
+export const signInUserController = new SignInUserController();
--- src/Routes/UserRouter.ts
+++ src/Routes/UserRouter.ts
@@ -1,12 +1,20 @@
-import { Router, Request, Response } from 'express'
-import { createUserController } from '../Controllers/User/CreateUserController'
-import { asyncHandler } from '../Error/Handler'
+import { Router, Request, Response } from 'express';
+import { createUserController } from '../Controllers/User/CreateUserController';
+import { asyncHandler } from '../Error/Handler';
+import { signInUserController } from '../Controllers/User/SignInUserController';
 
-const router = Router()
+const router = Router();
 
-router.route('/signUp')
-  .post(asyncHandler((request: Request, response: Response) => {
-    return createUserController.handle(request, response)
-  }))
+router.route('/signUp').post(
+  asyncHandler((request: Request, response: Response) => {
+    return createUserController.handle(request, response);
+  })
+);
 
-export default router
+router.route('/signIn').post(
+  asyncHandler((request: Request, response: Response) => {
+    return signInUserController.handle(request, response);
+  })
+);
+
+export default router;
